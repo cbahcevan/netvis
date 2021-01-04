ae9c5a0d057ce1fcc2c35705b71d46d2c0b7f2b9
kayhanliao <42952515+kayhanliao@users.noreply.github.com>
2021-01-04 21:07:31 +0300

    logo included

--- scatter/scatter_template.py
+++ scatter/scatter_template.py
@@ -15,6 +15,23 @@ body {
     font-family:"Helvetica Neue";
     color: #686765;
 }
+
+div#layout {
+  text-align: center;
+}
+
+div#container {
+  width: 1000px;
+  height: 600px;
+  margin:auto;
+  background-color: |backgroundcolor|;
+}
+
+svg {
+  width: 100%;
+  height: 100%;
+}
+
 .name {
     float:right;
     color:#27aae1;
@@ -40,12 +57,17 @@ text {
     opacity: .7;
     cursor: pointer;
 }
+g {
+  font-size: 18px
+}
+
 </style>
 """
 
 d3_js = """
 </head>
 <body>
+    <div id="scatter-load"></div>
   <script>
     var sample_data = |formatted_data|
 
@@ -53,20 +75,20 @@ showScatterPlot(sample_data);
 
 function showScatterPlot(data) {
   var margins = {
-    "left": 40,
+    "left": 60,
     "right": 30,
     "top": 30,
     "bottom": 30
   };
 
-  var width = 500;
-  var height = 500;
+  var width = 800;
+  var height = 800;
 
   var colors = d3.scaleOrdinal(d3.schemeCategory10);
 
   var svg = d3.select("#scatter-load").append("svg").attr("width", width).attr("height", height).append("g")
-    .attr("transform", "translate(" + margins.left + "," + margins.top + ")");
-
+    //.attr("transform", "translate(" + margins.left + "," + margins.top + ")");
+      .attr("transform", "translate(80,80)");
 
   var x = d3.scaleLinear()
     .domain(d3.extent(data, function(d) {
@@ -78,24 +100,39 @@ function showScatterPlot(data) {
     .domain(d3.extent(data, function(d) {
       return d.data_y;
     }))
-    .range([height - margins.top - margins.bottom, 0]);
+    .range([height - margins.top - margins.bottom - 20, 0]);
 
-  svg.append("g").attr("class", "x axis").attr("transform", "translate(0," + y.range()[0] + ")");
-  svg.append("g").attr("class", "y axis");
+  svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + y.range()[0] + ")")
+
+  svg.append("g")
+  .attr("class", "y axis");
 
   svg.append("text")
     .attr("fill", "#414241")
-    .attr("text-anchor", "end")
+    .style('font-size','16px')
+    .style("text-anchor", "middle")
     .attr("x", width / 2)
-    .attr("y", height - 35)
-    .text('|xtitle|');
+    .attr("y", height - 30)
+    .text("|xtitle|");
 
   svg.append("text")
+    .attr("transform","rotate(-90)")
     .attr("fill", "#414241")
-    .attr("text-anchor", "end")
-    .attr("x", -15)
-    .attr("y", height/2)
-    .text('|ytitle|');
+    .style('font-size','16px')
+    .style("text-anchor", "middle")
+    .attr("x", -height/2 + 40)
+    .attr("y", -30)
+    .text("|ytitle|");
+
+  svg.append('text')
+    .text('|title|')
+    .attr('x',width/2)
+    .attr('y', -margins.top)
+    .style('font-size','24px')
+    .style('fill','|titlecolor|')
+    .style("text-anchor", "middle");
 
 
   var xAxis = d3.axisBottom(x).tickPadding(2);
@@ -115,18 +152,18 @@ function showScatterPlot(data) {
     });
 
   dataGroup.append("circle")
-    .attr("r", 2)
+    .attr("r", 2.5)
     .attr("class", "dot")
     .style("fill", function(d) {
       return colors(d.type);
     });
 
-  dataGroup.append("text")
-    .style("text-anchor", "middle")
-    .attr("dy", -10)
-    .text(function(d) {
-      return d.name;
-    });
+//  dataGroup.append("text")
+//    .style("text-anchor", "middle")
+//    .attr("dy", -10)
+//    .text(function(d) {
+//      return d.name;
+//    });
 }
 
 
@@ -134,17 +171,25 @@ function showScatterPlot(data) {
 </script>
 </body>
 </html>
-
 """
 
-
-additional_bottom_text = """
-"""
 
 
 horizontal_lines_part = """
+    svg.append('g')
+      .attr('class', 'grid')
+      .call(makeYLines()
+        .tickSize(-width, 0, 0)
+        .tickFormat(''))
 """
 
 
 vertical_lines_part = """
+        svg.append('g')
+       .attr('class', 'grid')
+       .attr('transform', `translate(0, ${height})`)
+       .call(makeXLines()
+         .tickSize(-height, 0, 0)
+         .tickFormat('')
+       )
 """
